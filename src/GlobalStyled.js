b4a9923831e1e93a5b34a57638fe11ac7458a145
yeffweb <yeferson9828@gmail.com>
2021-10-12 09:50:57 -0500

    configuracion inicial

---
+++ src/GlobalStyled.js
@@ -0,0 +1,25 @@
+
+const GlobalStyled = {
+  breakPoints: {
+    xs: '376px',
+    lg: '1366px'
+  },
+  colors: {
+    primay: {
+      PaleBlue: 'hsl(225, 100%, 94%)',
+      BrightBlue: 'hsl(245, 75%, 52%)',
+    },
+    neutral: {
+      VeryPaleBlue: 'hsl(225, 100%, 98%)',
+      DesaturatedBlue: 'hsl(224, 23%, 55%)',
+      DarkBlue: 'hsl(223, 47%, 23%)',
+    }
+  },
+  fonts: {
+    medium: 500,
+    bold: 700,
+    black: 900
+  }
+}
+
+export default GlobalStyled
